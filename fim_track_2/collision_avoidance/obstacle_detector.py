Discovered that Turtlebots do not accept high acceleration cmd_vels. Add make profile code in pose.py to resolve this issue. Next: find out the correct way to do quaternion conversion between Optitrack system and our system. Test LQR waypoint tracking.

fim_track_2/collision_avoidance/obstacle_detector.py
@@ -42,5 +42,5 @@ class source_contact_detector:
 			src_loc = np.array([l.get_latest_loc() for l in self.sl if not l.get_latest_loc() is None]).reshape(-1,len(sensor_loc))
 			if src_loc is None:
 				return False
-			self.mc_node.get_logger().info(str(src_loc)+','+str(sensor_loc)+str(self.src_names))
+			# self.mc_node.get_logger().info(str(src_loc)+','+str(sensor_loc)+str(self.src_names))
 			return np.any(np.linalg.norm(src_loc-sensor_loc)<=0*BURGER_RADIUS)

fim_track_2/single_robot_controller.py
@@ -20,7 +20,7 @@ tools_root = os.path.join(os.path.dirname(__file__))
 sys.path.insert(0, os.path.abspath(tools_root))
 
 from ros2_utils.robot_listener import robot_listener
-from ros2_utils.pose import prompt_pose_type_string,turtlebot_twist, stop_twist
+from ros2_utils.pose import prompt_pose_type_string,bounded_change_update, turtlebot_twist, stop_twist
 
 from motion_control.WaypointTracking import LQR_for_motion_mimicry
 
@@ -80,6 +80,9 @@ class motion_control_node(Node):
 
 		self.source_contact_detector = source_contact_detector(self)
 
+		# current control actions
+		self.v = 0.0
+		self.omega = 0.0
 
 	def MOVE_CALLBACK(self,data):
 
@@ -88,8 +91,6 @@ class motion_control_node(Node):
 				self.get_logger().info('Robot Moving')
 			else:
 				self.get_logger().info('Robot Stopping')
-				self.vel_pub.publish(stop_twist())
-		
 
 		self.MOVE = data.data
 
@@ -100,7 +101,8 @@ class motion_control_node(Node):
 
 		
 	def timer_callback(self):
-		if self.MOVE:
+		# if self.MOVE:
+		if True:
 			if self.source_contact_detector.contact():
 				self.vel_pub.publish(stop_twist())
 				self.get_logger().info('Source Contact')
@@ -120,23 +122,36 @@ class motion_control_node(Node):
 					curr_x = np.array([loc[0],loc[1],yaw])		
 
 					self.control_actions = deque(get_control_action(free_space.project_point(self.waypoints),curr_x))
-
+					print(loc,yaw)
 				if len(self.control_actions)>0:
 
 					# Pop and publish the left-most control action.
-					[v,omega] = self.control_actions.popleft()
 
+					[v,omega] = self.control_actions.popleft()
 					print(v,omega)
-					print("Moving")
+					
+					[v,omega] = bounded_change_update(v,omega,self.v,self.omega) # Get a vel_msg that satisfies max change and max value constraints.
 					
 					vel_msg = turtlebot_twist(v,omega)
+
+					# Update current v and omega
+					self.v = v
+					self.omega = omega
+
 					self.vel_pub.publish(vel_msg)
 				else:
 					self.vel_pub.publish(stop_twist())
-					# print("Running out of control actions.")
+					print("Running out of control actions.")
+
+					# Update current v and omega
+					self.v = 0.0
+					self.omega = 0.0
+
 		else:
 			self.vel_pub.publish(stop_twist())
-
+			# Update current v and omega
+			self.v = 0.0
+			self.omega = 0.0
 		# self.MOVE=False # Crucial: reset self.MOVE to False everytime after new velocity is published, so that the robots will stop when remote loses connetion.
 	
 

fim_track_2/waypoint_planning.py
@@ -63,7 +63,7 @@ class waypoint_planning_node(Node):
 		# self.waypoints = []
 		
 		# Temporary hard-coded waypoints used in devel.	
-		self.waypoints = np.array([[4.,1],[5.,1],[6.,1]])
+		self.waypoints = np.array([[0.,1],[1.,1],[2.,1],[3,1]])
 
 		self.q_hat = []
 
@@ -134,35 +134,35 @@ class waypoint_planning_node(Node):
 		my_loc=self.get_my_loc()
 	
 
-		if (not my_loc is None) and len(self.q_hat)>0:
+		# if (not my_loc is None) and len(self.q_hat)>0:
 
-			neighbor_loc = self.get_strict_neighbor_locs()
+		# 	neighbor_loc = self.get_strict_neighbor_locs()
 
-			self.waypoints = WaypointPlanning.waypoints(self.q_hat,my_loc,neighbor_loc,lambda qhat,ps: self.dLdp(qhat,ps,FIM=self.FIM), step_size = self.sleep_time * BURGER_MAX_LIN_VEL)	
+		# 	self.waypoints = WaypointPlanning.waypoints(self.q_hat,my_loc,neighbor_loc,lambda qhat,ps: self.dLdp(qhat,ps,FIM=self.FIM), step_size = self.sleep_time * BURGER_MAX_LIN_VEL)	
 
 
-			# Publish the waypoints currently following.
-			out = Float32MultiArray()	
-			out.data = list(self.waypoints.ravel())
-			self.wp_pub.publish(out)
+		# 	# Publish the waypoints currently following.
+		# 	out = Float32MultiArray()	
+		# 	out.data = list(self.waypoints.ravel())
+		# 	self.wp_pub.publish(out)
 
-			# Consensus on the global FIM estimate.
-			newF = self.calc_new_F()
-			dF = newF-self.F
-			self.cons.timer_callback(dx=dF)
-			self.FIM = self.cons.get_consensus_val().reshape(self.FIM.shape)
-			self.F = newF
+		# 	# Consensus on the global FIM estimate.
+		# 	newF = self.calc_new_F()
+		# 	dF = newF-self.F
+		# 	self.cons.timer_callback(dx=dF)
+		# 	self.FIM = self.cons.get_consensus_val().reshape(self.FIM.shape)
+		# 	self.F = newF
 
-			print("publishing")
+		# 	print("publishing")
 
 
 			# self.get_logger().info(str(self.FIM))
 
 
 		# Publish the waypoints currently following.
-		# out = Float32MultiArray()	
-		# out.data = list(self.waypoints.ravel())
-		# self.wp_pub.publish(out)
+		out = Float32MultiArray()	
+		out.data = list(self.waypoints.ravel())
+		self.wp_pub.publish(out)
 
 
 	